572cdd066e45bcdd1484cd584a841dc47e90088e
fede shierson <fedesh_98@live.com.ar>
2022-07-14 18:54:46 -0300

    eliminando y agregando

--- main.js
+++ main.js
@@ -1,221 +1,70 @@
-/*let nombreDeUsuarios = prompt("Ingrese nombre de usuiario");
 
-if (nombreDeUsuarios == ""){ 
-
-    alert("nombre de usuario")
-} else {
-    alert("nombre de usuario ingresado: "+nombreDeUsuarios)
-}
-/*let numero = 1000;
-
-if( numero < 100){
-    alert("El numero es menor a 20");
-} else if ( numero < 50 ){
-    alert("el numero es menor a 50");
-} else if ( numero < 1000 ) {
-    alert("el numero es menor a 1000")
-} else {
-    alert ("numero es mayor a 1000")
-}*/
-
-/*for (let i = 0; i <= 5; i++){
-
-    alert(i)
-    }*/
-
-    /*for (let i = 1; i <= 5; i++) {
-        let ingreeasarNombre = prompt ("ingrese su nombre")
-        alert (turno N.-)
-    }*/
-
-   /*for (let i = 1; i <= 10; i++) {
-        if (i == 7 ) {
-            break;
-        }
-        alert (i)
-
-    }*/
-    /*for (let i = 1; i <= 10; i++) {
-        if (i == 5 ) {
-           continue;
-        }
-        alert (i)
-
-    }*/
-    /*let entrada = prompt("ingrese un doto");
-
-    while ( entrada != "ESC") {
-        alert("el usuario ingreso:"+entrada);
-        entrada = prompt("ingrese otro dato");
-    }*/
-
-    /*let numero = 0
-    do {
-        numero = prompt ("Ingrese un numero:");
-    }while (parseInt(numero));*/
-
-    //ejemplo de validacion de correo electronico
-    /*let askAgain = tru;
-    do {
-        let userEmail = prompt("ingrese su correo electronico:");
-        let userEmail = prompt("confirme su correo electronico:");
-        if ( userEmail === confirmUserEmail ) {
-            alert("el correo:"+userEmail+"asido guardado");
-            askAgain = false;
-        } else {
-            alert("los correos deben ser iguales");
-        }
-    } while (askAgain);*/
-
-    // mismo ejemplo
-    /*let askAgain = true;
-
-do {
-    let userEmail = prompt("Ingrese su correo electrónico:");
-    let confirmUserEmail = prompt("Confirme su correo electrónico:");
-
-    if ( userEmail === confirmUserEmail ) {
-        alert("El correo: "+userEmail+" a sido guardado con éxito!");
-        askAgain = false;
-    } else {
-        alert("Los correos deben de ser iguales");
-    }
-} while (askAgain);*/
-
-/*let producto ="";
-let precio = 0;
-let cantidad = 0;
-let precioTotal = 0;
-let seguirComprando = false;
-do{
-    producto = prompt ("¿queres comprar un buzo o figura?")
-    cantidad = Number ( prompt("¿cuantos productos?"));
-    switch(producto) {
-        case "buzo":
-        precio = 3000;
-        case "figura":
-            precio = 5000;
-            break;
-        case "ambos":
-            precio = 7000;
-            break;
-        default:
-            alert("algunos de los datos son incorrectos");
-            precio =0;
-            cantidad = 0;
-            break;
-    }
-    precioTotal += precio * cantidad;
-
-    seguirComprando = confirm("¿queres seguir comprando?");
-
-}while (seguirComprando);
-alert("el valor es :"+precioTotal)*/
-
-/*const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-
-const iva = x => x * 0.21;
-
-let precioProducto = 500;
-let precioDescuento = 50;
-
-let nuevoPrecio = resta( suma(precioProducto, iva(precioProducto)), precioDescuento);
-
-console.log(nuevoPrecio);
-
-
-
-/*function Producto(nombre, precio, marca) {
-    this.nombre = nombre,
-    this.precio = precio,
-    this.marca = marca
-}
-
-const producto1 = new Producto ("Remera", 100, "Adidas");*/
-
-/*let resultado = 0;
-function sumar (primerNumero, segundoNumero){
-    resultado = primerNumero + segundoNumero;
-}
-function mostrarResultado(resultado) {
-    console.log()
-}
-sumar(1000, 2000);
-mostrarResultado(resultado);
-console.log(resultado)*/
-
-/*const numeros = [15,30,45,60,75]
-for (let i = 0; i < numeros.length; i++ ) {
-    alert (numeros [i])
-    
-}*/
-
-
-
-/*const aves = ["pepo","pepi"];
-const gatos = ["michu","panterita","michina"];
-const mascotas = aves.concat(gatos);
-console.log(mascotas);
-
-const otroArray = ["hola", "como","estas"]
-console.log(otroArray.join("-"));*/
-
-/*const nombres = ["fede", "miguel", "silvia", "gustavo", "noemi"];
-let nuevoUsuario = "";
-do {
-    if (nuevoUsuario != "" ) {
-        alert("el nombre de usuario ya existe, ingrese uno nuevo");
-    }
-    nuevoUsuario = prompt ("ingrese un nombre de usuario:");
-} while ( nombres.indexOf(nuevoUsuario) != -1);
-alert("el nombre de usuario "+nuevoUsuario+" se creo");*/
-
-/*const carritoDeCompras = [
-    {nombre: 'Buzos', precio: 2000, cantidad:1},
-    {nombre: 'remeras', precio: 1500, cantidad: 3},
-    {nombre: 'figura', precio: 5000, cantidad:1}
-];
-const cantidadTotal = carritoDeCompras.reduce( (acc, producto) => acc + producto.cantidad, 0);
-const precioTotal = carritoDeCompras.reduce( (acc, elemento) => acc + elemento.precio * elemento.cantidad, 0);
-console.log(cantidadTotal);
-console.log(precioTotal);*/
-
-
-/*let producto ="";
-let precio = 0;
-let cantidad = 0;
-let precioTotal = 0;
-let seguirComprando = false;
-do{
-    producto = prompt ("¿queres comprar un buzo o figura?")
-    cantidad = Number ( prompt("¿cuantos productos?"));
-    switch(producto) {
-        case "buzo":
-        precio = 3000;
-        case "figura":
-            precio = 5000;
-            break;
-        case "ambos":
-            precio = 7000;
-            break;
-        default:
-            alert("algunos de los datos son incorrectos");
-            precio =0;
-            cantidad = 0;
-            break;
-    }
-    precioTotal += precio * cantidad;
-
-    seguirComprando = confirm("¿queres seguir comprando?");
-
-}while (seguirComprando);
-alert("el valor es :"+precioTotal)*/
 let titulo = document.getElementById("Dom")
 
 const nombredeUsuario = prompt ("Bienvenido! ingrese su nombre de usuario:");
  titulo.innerText = "Hola " +nombredeUsuario+ "!";
 
 
+
+ const boton = document.getElementById("compra");
+ 
+ boton.addEventListener("click", respuestaClick);
+
+ function respuestaClick() {
+    console.log("respuesta al evento click")
+ }
+
+ const element = document.querySelector('.botones');
+
+ element.addEventListener("click", modifyText);
+
+ function modifyText () {
+    let carrito = document.querySelector('.carrito')
+    carrito.innerText = "ya fue agregado"
+ }
+
+ const productos = [
+   {id: 1 , nombre: "buzo", precio: 3000},
+   {id: 2 , nombre: "buzo", precio: 3000},
+   {id: 3 , nombre: "buzo", precio: 3000},
+   {id: 4 , nombre: "buzo", precio: 3000},
+   {id: 5 , nombre: "buzo", precio: 3000},
+   {id: 6 , nombre: "buzo", precio: 3000},
+   {id: 7 , nombre: "buzo", precio: 3000},
+   {id: 8 , nombre: "buzo", precio: 3000},
+   {id: 9 , nombre: "buzo", precio: 3000},
+   {id: 10 , nombre: "buzo", precio: 3000},
+   {id: 11 , nombre: "buzo", precio: 3000},
+   {id: 12 , nombre: "buzo", precio: 3000},
+   {id: 13 , nombre: "buzo", precio: 3000},
+   {id: 14 , nombre: "buzo", precio: 3000},
+   {id: 15 , nombre: "buzo", precio: 3000},
+   {id: 16 , nombre: "buzo", precio: 3000},
+   {id: 17 , nombre: "remera", precio: 2000},
+   {id: 18 , nombre: "remera", precio: 2000},
+   {id: 19 , nombre: "remera", precio: 2000},
+   {id: 20 , nombre: "remera", precio: 2000},
+   {id: 21 , nombre: "remera", precio: 2000},
+   {id: 22 , nombre: "remera", precio: 2000},
+   {id: 23 , nombre: "remera", precio: 2000},
+   {id: 24 , nombre: "remera", precio: 2000},
+   {id: 25 , nombre: "remera", precio: 2000},
+   {id: 26 , nombre: "remera", precio: 2000},
+   {id: 27 , nombre: "remera", precio: 2000},
+   {id: 28 , nombre: "remera", precio: 2000},
+   {id: 29 , nombre: "remera", precio: 2000},
+   {id: 30 , nombre: "remera", precio: 2000},
+ ];
+ const guardarLocal = (clave, valor) =>{
+   localStorage.setItem (clave, valor);
+ };
+
+ //guardarLocal('carrito', JSON.stringify(productos));
+for (const productos of productos) {
+   guardarLocal( productos.nombre, JSON.stringify(productos))
+}
+
+
+ 
 
 
