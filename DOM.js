52d9b7574205f78a15b8aa049c091a718e19433b
fede shierson <fedesh_98@live.com.ar>
2022-09-02 20:22:02 -0300

    dom

--- DOM.js
+++ DOM.js
@@ -1,11 +1,14 @@
 //ALL = cuando queremos traer muchos elementos 
+//${} = me permite colocar codigos dentro de los strings
 
 
-
-    const newElement = document.createElement("div");
-    document.querySelector("#main-grid").append(newElement);
-    newElement.innerHTML = '<img src="../imagenes/figura_trunks4.jpg" alt="remera">, <h2> Figura Trunks </h2>, <p> Precio:</p>';
-    newElement.classList.add("content-div");
+    for (let index = 0; index < 4; index++) {
+      
+      const newElement = document.createElement("div");
+      document.querySelector("#main-grid").append(newElement);
+      newElement.innerHTML = '<img src="../imagenes/figura_trunks4.jpg" alt="remera">, <h2> Figura Trunks </h2>, <p> Precio:</p>';
+      newElement.classList.add("content-div");
+    }
 
 
 
