a0c50ae7a1d8bf80abffcd1defa70c2e4fcc3158
fede shierson <fedesh_98@live.com.ar>
2022-09-02 09:33:41 -0300

    Dom

--- DOM.js
+++ DOM.js
@@ -4,9 +4,7 @@
 
     const newElement = document.createElement("div");
     document.querySelector("#main-grid").append(newElement);
-    newElement.innerHTML = '<img src="../imagenes/figura_trunks4.jpg" alt="remera">'
-    '<h2> remera </h2>'
-    '<p> Precio:</p>';
+    newElement.innerHTML = '<img src="../imagenes/figura_trunks4.jpg" alt="remera">, <h2> Figura Trunks </h2>, <p> Precio:</p>';
     newElement.classList.add("content-div");
 
 
