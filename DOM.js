cabd96c851d59439df8723c695b8926c6dcf36f1
fede shierson <fedesh_98@live.com.ar>
2022-09-03 10:53:49 -0300

    dom

--- DOM.js
+++ DOM.js
@@ -1,12 +1,18 @@
 //ALL = cuando queremos traer muchos elementos 
 //${} = me permite colocar codigos dentro de los strings
+// para hacer el acento invertido (``) es Ctrl + Alt dos veces
 
-
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 3; index++) {
       
       const newElement = document.createElement("div");
       document.querySelector("#main-grid").append(newElement);
-      newElement.innerHTML = '<img src="../imagenes/figura_trunks4.jpg" alt="remera">, <h2> Figura Trunks </h2>, <p> Precio:</p>';
+      newElement.innerHTML = `<img src="../imagenes/figura_broly3.jpg" alt="remera">
+      <h2>
+        remera
+      </h2>
+      <p>
+        Precio:
+      </p>`
       newElement.classList.add("content-div");
     }
 
