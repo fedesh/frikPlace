da7f2d7d00ee32db554e04c553ba610a9ce0edf6
fede shierson <fedesh_98@live.com.ar>
2022-09-04 00:07:00 -0300

    dom

--- DOM.js
+++ DOM.js
@@ -1,7 +1,7 @@
 //ALL = cuando queremos traer muchos elementos 
 //${} = me permite colocar codigos dentro de los strings
 // para hacer el acento invertido (``) es Ctrl + Alt dos veces
-
+    let BrolyF = [""]
     for (let index = 0; index < 3; index++) {
       
       const newElement = document.createElement("div");
@@ -11,7 +11,7 @@
         remera
       </h2>
       <p>
-        Precio:
+        nomrbe
       </p>`
       newElement.classList.add("content-div");
     }
