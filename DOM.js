934580504e84107d2dd1cc2260dc2b4831a12ed6
fede shierson <fedesh_98@live.com.ar>
2022-09-05 11:26:43 -0300

    dom

--- DOM.js
+++ DOM.js
@@ -2,19 +2,22 @@
 //${} = me permite colocar codigos dentro de los strings
 // para hacer el acento invertido (``) es Ctrl + Alt dos veces
 
-const image = document.createElement('img');
-image.src = '/imagenes/figura_freezer.jpg';
-
+let image = new Image (300, 300);
+ image.src = "./imagenes/yamcha.jpg;"
+let alt = document.createElement('alt')
+  alt = "figura"
+let yamcha = document.createElement('h2')
+   yamcha = "yamcha"
+   console.log(image)
 for (let index = 0; index < 3; index++) {
-      
-      const newElement = document.createElement("div");
-      document.querySelector("#main-grid").append(newElement);
-      newElement.innerHTML = `${image};
+    const newElement = document.createElement("div");
+    document.querySelector("#main-grid").append(newElement);
+      newElement.innerHTML = `<img src= "${image}" ${alt}>
       <h2>
-        remera
+        ${yamcha}
       </h2>
       <p>
-        nomrbe
+        nombre
       </p>`
       newElement.classList.add("content-div");
     }
