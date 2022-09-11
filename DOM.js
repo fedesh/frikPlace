3376fa62ea8db7ecaa2f7fb842e035a9c5e02222
fede shierson <fedesh_98@live.com.ar>
2022-09-11 00:24:23 -0300

    seguimos en dom

--- DOM.js
+++ DOM.js
@@ -2,29 +2,53 @@
 //${} = me permite colocar codigos dentro de los strings
 // para hacer el acento invertido (``) es Ctrl + Alt dos veces
 
-let linkImage = '"../imagenes/yamcha.jpg"'
-let image =`<img src=${linkImage} />`
- image.src = "./imagenes/yamcha.jpg;"
+
+
+
+ let linkImage = '"../imagenes/yamcha.jpg"'
+ let image =`<img src=${linkImage} />`
+
+
+ 
+ 
+ let linkImage2 = '"../imagenes/figura_broly4.jpg"'
+ let image2 = `<img src=${linkImage2} />`
+
+
 let alt = document.createElement('alt')
   alt = "figura"
 let yamcha = document.createElement('h2')
    yamcha = "yamcha"
-let yamcha2 = document.createElement('p')
-   yamcha2 = "figura"
+let figuras = document.createElement('p')
+   figuras = "Figura"
    
 for (let index = 0; index < 3; index++) {
-    const newElement = document.createElement("div");
-    document.querySelector("#main-grid").append(newElement);
-      newElement.innerHTML = `${image}
-      <h2>
-        ${yamcha}
-      </h2>
-      <p>
-        ${yamcha2}
-      </p>`
-      newElement.classList.add("content-div");
-    }
+  
+  
+  const newElement = document.createElement("div");
+  document.querySelector("#main-grid").append(newElement);
+  newElement.innerHTML = `${image}
+  <h2>
+  ${yamcha}
+  </h2>
+  <p>
+  ${figuras}
+  </p>`
+  newElement.classList.add("content-div");
+}
 
 
-
-    
+for (let index = 0; index < 1; index++) {
+  
+  
+  const newElement = document.createElement("div");
+  document.querySelector("#main-grid").append(newElement);
+  newElement.innerHTML = `${image2}
+  <h2>
+  ${yamcha}
+  </h2>
+  <p>
+  ${figuras}
+  </p>`
+  newElement.classList.add("content-div");
+} 
