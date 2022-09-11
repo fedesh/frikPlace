05e9919de0a58eeff11802abb978dc08e8b1feac
fede shierson <fedesh_98@live.com.ar>
2022-09-11 10:52:51 -0300

    eventos

--- eventos.js
+++ eventos.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
     console.log ("el documento se a cargado" )
    
 });
@@ -7,4 +7,23 @@ const boton = document.querySelector('.carrito');
 boton.addEventListener('click', () => {
     console.log("click");
 
-});
+});*/
+
+
+
+
+const botonnes = document.querySelectorAll('.carrito')
+const carrito = document.getElementById('main-grid')
+
+carrito.addEventListener('click',(e)=>{
+    console.log(e.target.classList.contains('carrito'))
+    let value = e.target.classList.contains('carrito') 
+
+    if(value){
+        e.target.classList.toggle('changeColor')
+    }
+
+
+     
+    e.stopImmediatePropagation();
+})
