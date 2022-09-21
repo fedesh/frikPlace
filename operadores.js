fc12f658595d61e2dfb21df692de8b6f3d06ecf9
fede shierson <fedesh_98@live.com.ar>
2022-09-21 18:06:49 -0300

    librerias

--- operadores.js
+++ operadores.js
@@ -6,8 +6,13 @@ const usuario = {
 };
 
 const registroIngreso = usuario.edad >= 18 && new Date();
-//getDate = el es dia del mes
+//getDate = el es dia del mes 
 console.log(new Date().getDate());
 //getDay= es el dia de la semana
 console.log(new Date().getDay());
 console.log(registroIngreso)
+
+let user ;
+console.log(usuario.nombre || "el usuario no existe")
+//usamos el "?" para que no se rompa y seguir usando el codigo
+console.log(usuario?.nombre || "el usuario no existe")
